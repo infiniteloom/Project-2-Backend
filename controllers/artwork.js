56bcbe628dd9d1275d4deec9feb706d4bd21f1d0
hamai <hamai@hamais-MacBook-Pro.local>
2020-08-06 15:30:14 -0400

    updating update request

--- controllers/artwork.js
+++ controllers/artwork.js
@@ -79,7 +79,7 @@ const create = async (req, res)=>{
 // Update Artwork Route
 const update = async (req, res) => {
     try {
-        const updatedArtwork = await Artwork.findByIdAndUpdate(req.params.id)    
+        const updatedArtwork = await Artwork.findByIdAndUpdate(req.params.id, req.body)    
         res.status(200).json(updatedArtwork)
     }
     catch(error){
